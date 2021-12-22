a41ebee9c9043d1848f7aab9a22d3f58ffee32ae
K-mel <63577892+K-mel@users.noreply.github.com>
2021-12-22 10:11:01 +0100

    Implement map fragment

--- app/src/main/java/com/camel/go4lunch/ui/MainActivity.java
+++ app/src/main/java/com/camel/go4lunch/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.camel.go4lunch.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -11,34 +12,34 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.camel.go4lunch.ui.MainActivityViewModel;
+import com.facebook.login.LoginManager;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.camel.go4lunch.R;
+import com.camel.go4lunch.databinding.ActivityMainBinding;
 import com.camel.go4lunch.databinding.ActivityMainDrawerHeaderBinding;
 import com.camel.go4lunch.injection.Injection;
 import com.camel.go4lunch.injection.ViewModelFactory;
 import com.camel.go4lunch.models.Workmate;
 import com.camel.go4lunch.ui.fragment.LoginFragmentViewModel;
 
-import com.camel.go4lunch.R;
-import com.facebook.login.LoginManager;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.camel.go4lunch.databinding.ActivityMainBinding;
-
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, FirebaseAuth.AuthStateListener {
 
     private MainActivityViewModel mViewModel;
     private ActivityMainBinding mBinding;
     private ActivityMainDrawerHeaderBinding mHeaderBinding;
-
     private NavController mNavController;
     private BottomNavigationView mBottomNavigationView;
     private DrawerLayout mDrawerLayout;
@@ -59,7 +60,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         configureNavController();
         configureBottomNavigation();
         configureToolbar();
+        configureDrawer();
         configureNavControllerListener();
+        configureFirebaseAuthListener();
+    }
+
+    private void configureViewModel() {
+        ViewModelFactory viewModelFactory = Injection.provideViewModelFactory();
+        mViewModel = new ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel.class);
+    }
+
+    private void workmateObserver(Workmate workmate){
+        updateDrawerHeader(workmate);
     }
 
     private void configureViewModel() {
@@ -68,44 +80,35 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         mViewModel.getWorkmateWithId(user.getUid()).observe(this, this::workmateObserver);
     }
-
     private void workmateObserver(Workmate workmate){
         updateDrawerHeader(workmate);
     }
-
     private void configureNavController() {
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.main_activity_nav_host);
         assert navHostFragment != null;
         mNavController = navHostFragment.getNavController();
     }
-
     private void configureBottomNavigation() {
         mBottomNavigationView = mBinding.bottomNavigationView;
         NavigationUI.setupWithNavController(mBottomNavigationView, mNavController);
     }
-
     private void configureToolbar() {
         setSupportActionBar(mToolbar);
         mToolbar = mBinding.mainActivityToolbar;
         mToolbar.setTitle(getString(R.string.i_m_hungry));
         AppBarConfiguration appBarConfiguration =
-               new AppBarConfiguration.Builder(R.id.map_view_fragment, R.id.list_view_fragment, R.id.workmates_fragment)
+                new AppBarConfiguration.Builder(R.id.map_view_fragment, R.id.list_view_fragment, R.id.workmates_fragment)
                         .setOpenableLayout(mBinding.getRoot())
                         .build();
-
         NavigationUI.setupWithNavController( mToolbar, mNavController, appBarConfiguration);
     }
-
     private void configureDrawer() {
         NavigationView navigationView = findViewById(R.id.activity_main_nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
         mDrawerLayout = mBinding.activityMainDrawerLayout;
     }
-
     private void configureNavControllerListener() {
         mNavController.addOnDestinationChangedListener((controller, destination, arguments) -> {
-
             if(destination.getId() == R.id.login_fragment) {
                 mToolbar.setVisibility(View.GONE);
                 mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
@@ -122,23 +125,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
     }
-
     private void updateDrawerHeader(Workmate workmate) {
         Glide.with(this).load(workmate.getPictureUrl()).apply(RequestOptions.circleCropTransform()).into(mHeaderBinding.drawerProfilePicIv);
         mHeaderBinding.drawerNameTv.setText(workmate.getUserName());
         mHeaderBinding.drawerEmailTv.setText(workmate.getEmail());
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //2 - Inflate the menu and add it to the Toolbar
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return true;
     }
-
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-
         if(item.getItemId() == R.id.restaurant_details_fragment){
             mNavController.navigate(R.id.action_global_restaurant_details_fragment);
         }
@@ -149,10 +148,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             logout();
             mDrawerLayout.closeDrawer(GravityCompat.START);
         }
-
         return true;
     }
-
     private void logout() {
         FirebaseAuth.getInstance().signOut();
         LoginManager.getInstance().logOut();
@@ -160,4 +157,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mNavController.navigate(R.id.action_global_login_fragment);
     }
 
+    private void configureFirebaseAuthListener() {
+        FirebaseAuth.getInstance().addAuthStateListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        FirebaseAuth.getInstance().removeAuthStateListener(this);
+    }
+
+    @Override
+    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser != null){
+            mViewModel.getWorkmateWithId(firebaseUser.getUid()).observe(this, this::workmateObserver);
+        }
+    }
 }
---
+++ app/src/main/java/com/camel/go4lunch/ui/fragment/MapViewFragment.java
@@ -0,0 +1,145 @@
+package com.camel.go4lunch.ui.fragment;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import com.camel.go4lunch.BaseFragment;
+import com.camel.go4lunch.R;
+
+import com.camel.go4lunch.databinding.FragmentMapViewBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.List;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class MapViewFragment extends BaseFragment implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback, EasyPermissions.PermissionCallbacks {
+
+    private static final int RC_LOCATION = 1;
+
+    private FragmentMapViewBinding mBinding;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private GoogleMap mMap;
+
+    private Boolean mPermissionDenied = false;
+
+    public MapViewFragment() {
+    }
+
+    public static MapViewFragment newInstance() {
+        return new MapViewFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        mBinding = FragmentMapViewBinding.inflate(getLayoutInflater());
+        mBinding.mapViewFragmentLocationBtn.setOnClickListener(v -> requestFocusToLocation());
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_view_fragment_map);
+        mapFragment.getMapAsync(this);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        enableLocation();
+    }
+
+    @SuppressLint("MissingPermission")
+    @AfterPermissionGranted(RC_LOCATION)
+    private void enableLocation() {
+        if (EasyPermissions.hasPermissions(getActivity(), ACCESS_FINE_LOCATION)) {
+            mPermissionDenied = false;
+            if (mMap != null) {
+                mMap.setMyLocationEnabled(true);
+                focusToLocation();
+            }
+        } else {
+            EasyPermissions.requestPermissions(this, getString(R.string.permission_rationale_location), RC_LOCATION, ACCESS_FINE_LOCATION);
+        }
+    }
+
+    private void requestFocusToLocation(){
+        if (mPermissionDenied && EasyPermissions.somePermissionPermanentlyDenied(getActivity(), Collections.singletonList(ACCESS_FINE_LOCATION))) {
+            new AppSettingsDialog.Builder(this).build().show();
+        } else if(EasyPermissions.hasPermissions(getActivity(), ACCESS_FINE_LOCATION)) {
+            focusToLocation();
+        } else {
+            EasyPermissions.requestPermissions(this, getString(R.string.permission_rationale_location), RC_LOCATION, ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void focusToLocation() {
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(location -> {
+            if (location != null) {
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 16);
+                mMap.animateCamera(cameraUpdate);
+            }
+        });
+    }
+
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        mPermissionDenied = false;
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        mPermissionDenied = true;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NotNull String[] permissions, @NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE && EasyPermissions.hasPermissions(getActivity(), ACCESS_FINE_LOCATION)) {
+            mPermissionDenied = false;
+            enableLocation();
+        }
+    }
+}
