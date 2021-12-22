036a6cfdb2c409065b613994da928a8032b30369
K-mel <63577892+K-mel@users.noreply.github.com>
2021-12-22 13:44:35 +0100

    Remove OkHTTP

--- app/src/main/java/com/camel/go4lunch/api/GooglePlacesService.java
+++ app/src/main/java/com/camel/go4lunch/api/GooglePlacesService.java
@@ -4,8 +4,6 @@ package com.camel.go4lunch.api;
 import com.camel.go4lunch.models.GooglePlaceResult.GooglePlaceResults;
 
 import io.reactivex.Observable;
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -16,12 +14,9 @@ public interface GooglePlacesService {
     @GET("/maps/api/place/nearbysearch/json")
     Observable<GooglePlaceResults> getNearbyPlaces(@Query("key") String apiKey, @Query("location") String latlng, @Query("radius") String radius, @Query("type") String type);
 
-    //HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
-
     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("https://maps.googleapis.com")
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            //.client(new OkHttpClient.Builder().addInterceptor(interceptor).build())
             .build();
 }
